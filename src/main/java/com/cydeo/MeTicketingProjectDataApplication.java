b594bf8e3738c3a0e2115da4da896beb061a5c17
aysegul <aysegulkafaliyilmaz@gmail.com>
2023-10-01 23:25:13 +0300

    Initial commit

---
+++ src/main/java/com/cydeo/MeTicketingProjectDataApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeTicketingProjectDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeTicketingProjectDataApplication.class, args);
+    }
+
+}
