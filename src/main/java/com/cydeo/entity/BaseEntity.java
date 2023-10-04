cbec785a6b7ef7444a77d69d370da0b6876ce0dd
aysegul <aysegulkafaliyilmaz@gmail.com>
2023-10-04 20:10:43 +0300

    part-3

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -2,10 +2,7 @@ package com.cydeo.entity;
 
 import lombok.*;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 @Getter
 @Setter
@@ -23,11 +20,19 @@ public class BaseEntity {
     private LocalDateTime lastUpdateDateTime;
     private Long lastUpdateUserId;
 
+    @PrePersist
     private void onPrePersist(){
         this.insertDateTime = LocalDateTime.now();
         this.lastUpdateDateTime = LocalDateTime.now();
         this.insertUserId = 1L;
         this.lastUpdateUserId = 1L;
+    }
+
+    @PreUpdate
+    private  void  onPreUpdate(){
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+
     }
 
 }
