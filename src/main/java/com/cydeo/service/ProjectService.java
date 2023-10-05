8e29946b9d51f5c40c1b1c50f94981897b1fe0a0
aysegul <aysegulkafaliyilmaz@gmail.com>
2023-10-05 17:58:45 +0300

    part-3

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 public interface ProjectService {
 
-    ProjectDTO getByProjectCode(String code);
+   ProjectDTO getByProjectCode(String code);
     List<ProjectDTO> listAllProjects();
     void save(ProjectDTO dto);
     void update(ProjectDTO dto);
     void delete(String code);
     void complete(String projectCode);
 
-
 }
+
