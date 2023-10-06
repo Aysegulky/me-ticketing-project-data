5f95e0fcef07243ffcedbe9495618f627d28d466
aysegul <aysegulkafaliyilmaz@gmail.com>
2023-10-07 02:38:34 +0300

    part-3

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -2,6 +2,7 @@ package com.cydeo.service;
 
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 
 import java.util.List;
 
@@ -20,6 +21,9 @@ public interface TaskService {
     void deleteByProject(ProjectDTO projectDTO);
 
     void completeByProject(ProjectDTO projectDTO);
+
+    List<TaskDTO> listAllTasksByStatusIsNot(Status status);
+    List<TaskDTO> listAllTasksByStatus(Status status);
 
 
 
