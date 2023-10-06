48f37ce4e75c24a85c829f468d72842fe909b8e0
aysegul <aysegulkafaliyilmaz@gmail.com>
2023-10-06 16:48:12 +0300

    part-3

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -13,27 +13,27 @@ import javax.validation.Valid;
 @RequestMapping("/task")
 public class TaskController {
 
-//    private final UserService userService;
-//    private final ProjectService projectService;
-//    private final TaskService taskService;
-//
-//    public TaskController(UserService userService, ProjectService projectService, TaskService taskService) {
-//        this.userService = userService;
-//        this.projectService = projectService;
-//        this.taskService = taskService;
-//    }
-//
-//    @GetMapping("/create")
-//    public String createTask(Model model) {
-//
-//        model.addAttribute("task", new TaskDTO());
-//        model.addAttribute("projects", projectService.findAll());
-//        model.addAttribute("employees", userService.findEmployees());
-//        model.addAttribute("tasks", taskService.findAll());
-//
-//        return "/task/create";
-//
-//    }
+    private final UserService userService;
+    private final ProjectService projectService;
+    private final TaskService taskService;
+
+    public TaskController(UserService userService, ProjectService projectService, TaskService taskService) {
+        this.userService = userService;
+        this.projectService = projectService;
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/create")
+    public String createTask(Model model) {
+
+        model.addAttribute("task", new TaskDTO());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks", taskService.findAll());
+
+        return "/task/create";
+
+    }
 //
 //    @PostMapping("/create")
 //    public String insertTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
